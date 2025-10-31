3fb855d0255c90a31e9c5b1917321678d3ecda75
sheikhabdullahahmed <sheikhabdullah456677@gmail.com>
2025-10-31 11:07:31 +0500

    Fix case sensitivity issue

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import "./App.css";
-import Login  from "./pages/Login.jsx";
+import {Login}  from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Home from "./pages/Dashboard/Home.jsx";
 import Income from "./pages/Dashboard/Income.jsx";
